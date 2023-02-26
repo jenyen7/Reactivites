a0b9644544a102274a73c958610b63fa63e88b4c
Jen <tryphenayen@gmail.com>
2023-02-27 01:35:48 +0800

    End of section 10

--- API/Controllers/ActivitiesController.cs
+++ API/Controllers/ActivitiesController.cs
@@ -1,24 +1,21 @@
 using Application.Activities;
 using Domain;
-using MediatR;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.EntityFrameworkCore;
-using Persistence;
 
 namespace API.Controllers
 {    
     public class ActivitiesController : BaseApiController
     {        
         [HttpGet] //api/activities
-        public async Task<ActionResult<List<Activity>>> GetActivities(CancellationToken ct)
+        public async Task<IActionResult> GetActivities(CancellationToken ct)
         {
-            return await Mediator.Send(new List.Query(), ct);
+            return HandleResult(await Mediator.Send(new List.Query(), ct));
         }
 
         [HttpGet("{id}")] //api/activities/123
-        public async Task<ActionResult<Activity>> GetActivity (Guid id)
-        {
-            return await Mediator.Send(new Details.Query{Id = id});
+        public async Task<IActionResult> GetActivity (Guid id)
+        {           
+            return HandleResult(await Mediator.Send(new Details.Query{Id = id}));
         }
 
         [HttpPost]
@@ -31,13 +28,13 @@ namespace API.Controllers
         public async Task<IActionResult> EditActivity(Guid id, Activity activity)
         {
             activity.Id = id;
-            return Ok(await Mediator.Send(new Edit.Command {Activity = activity}));
+            return HandleResult(await Mediator.Send(new Edit.Command {Activity = activity}));
         }
 
         [HttpDelete("{id}")]
         public async Task<IActionResult> DeleteActivity(Guid id)
         {
-            return Ok(await Mediator.Send(new Delete.Command {Id = id}));
+            return HandleResult(await Mediator.Send(new Delete.Command {Id = id}));
         }
     }
 }
--- Application/Activities/List.cs
+++ Application/Activities/List.cs
@@ -1,3 +1,4 @@
+using Application.Core;
 using Domain;
 using MediatR;
 using Microsoft.EntityFrameworkCore;
@@ -8,20 +9,18 @@ namespace Application.Activities
 {
     public class List
     {
-        public class Query : IRequest<List<Activity>> {}
+        public class Query : IRequest<Result<List<Activity>>> {}
 
-        public class Handler : IRequestHandler<Query, List<Activity>>
+        public class Handler : IRequestHandler<Query, Result<List<Activity>>>
         {
-            private readonly DataContext _context;
-            // private readonly ILogger<List> _logger;
+            private readonly DataContext _context;           
 
             public Handler(DataContext context, ILogger<List> logger)
-            {
-                // _logger = logger;
+            {               
                 _context = context;
             }
 
-            public async Task<List<Activity>> Handle(Query request, CancellationToken cancellationToken)
+            public async Task<Result<List<Activity>>> Handle(Query request, CancellationToken cancellationToken)
             {
                 // try
                 // {
@@ -37,7 +36,7 @@ namespace Application.Activities
                 //     _logger.LogInformation("Task was cancelled");
                 // }
 
-                return await _context.Activities.ToListAsync();
+                return Result<List<Activity>>.Success(await _context.Activities.ToListAsync());
             }
         }
     }
